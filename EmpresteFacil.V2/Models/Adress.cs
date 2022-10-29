0283b7b9d60c90cb97a96b1799c7b333f923c33d
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-10-29 18:40:33 -0300

    First commit

---
+++ EmpresteFacil.V2/Models/Adress.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmpresteFacil.V2.Models
+{
+    [Table("Enderecos")]
+    public class Adress
+    {
+        [Key]
+        [Display(Name = "Endereco")]
+        [Required(ErrorMessage = "Por favor, informe seu endereco")]
+        public string Endereco { get; set; }
+
+        [Required]
+        [Display(Name = "Complemento")]
+        public string EnderecoComplemento { get; set; }
+
+        [Required]
+        [Display(Name = "Bairro")]
+        public string Bairro { get; set; }
+
+        [Required]
+        [Display(Name = "Cidade")]
+        public string Cidade { get; set; }
+
+        [Required]
+        [Display(Name = "Cep")]
+        public string Cep { get; set; }
+
+        [Required]
+        [Display(Name = "Estado")]
+        public string Estado { get; set; }
+
+        [Required]
+        [Display(Name = "Pais")]
+        public string? Pais { get; set; }
+    }
+}
--- EmpresteFacil.V2/Models/UserBussines.cs
+++ EmpresteFacil.V2/Models/UserBussines.cs
@@ -1,6 +1,29 @@
-﻿namespace EmpresteFacil.V2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmpresteFacil.V2.Models
 {
-    public class UserBussines
+    [Table("Usuarios Pessoa Judirica")]
+    public class UserBussines : User
     {
+        [Key]
+        [Display(Name = "CNPJ")]
+        public string CNPJ { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe sua Razao Social")]
+        [StringLength(250, MinimumLength = 3, ErrorMessage = "A razao social deve ter entre 3 e 250 caracteres")]
+        public string RazaoSocial { get; set; }
+
+        [Display(Name = "Data de constituicao ou inicio das atividades")]
+        [DataType(DataType.DateTime, ErrorMessage = "Formato de data invalido")]
+        public DateTime DataConstituicao { get; set; }
+
+        public UserBussines(string cNPJ, string razaoSocial, DateTime dataConstituicao, int userId, string email, string celPhone, string homePhone) 
+            : base(userId, email, celPhone, homePhone)
+        {
+            CNPJ = cNPJ;
+            RazaoSocial = razaoSocial;
+            DataConstituicao = dataConstituicao;
+        }
     }
 }
