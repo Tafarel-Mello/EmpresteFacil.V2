4f6c22322f0dcfd036272c8238696df09a982531
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-10-29 19:22:54 -0300

    Feat: Add Models

---
+++ EmpresteFacil.V2/Models/Installments.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+
+namespace EmpresteFacil.V2.Models
+{
+    [Table("Parcelas")]
+    internal class Installments
+    {
+        [Key]
+        public int InstallmentsId { get; set; }
+
+        [Display(Name = "Valor da parcela")]
+        [Column(TypeName = "decimal(10 ,2)")]
+        public double InstallmentsValue { get; set; }
+
+        [Display(Name = "Data de vencimento da parcela")]
+        [DataType(DataType.DateTime)]
+        public DateTime InstallmentDueDate { get; set; }
+
+        [Display(Name = "Taxa de juros")]
+        [Column(TypeName = "decimal(10, 2)")]
+        public double Fees { get; set; }
+
+        [Display(Name = "Valor da amortizacao")]
+        [Column(TypeName = "decimal(10, 2)")]
+        public double Amortization { get; set; }
+
+        [Display(Name = "Data de pagamento")]
+        [DataType(DataType.DateTime)]
+        public DateTime PayDay { get; set; }
+
+        [Display(Name = "Valor de multa")]
+        [Column(TypeName = "decimal(10 ,2)")]
+        public double FineAmount { get; set; }
+
+        public enum InstallmentsStatus 
+        {
+            Pago,
+            Atraso
+        }
+
+        public int LoanId { get; set; }
+        public virtual Loan Loan { get; set; }
+    }
+}
---
+++ EmpresteFacil.V2/Models/Loan.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmpresteFacil.V2.Models
+{
+    [Table("Emprestimos")]
+    public class Loan
+    {
+        [Key]
+        public int LoanId { get; set; }
+
+        [Required(ErrorMessage = "Informe o valor desejado")]
+        [Column(TypeName = "decimal(10, 2)")]
+        public double LoanValue { get; set; }
+
+        [Required(ErrorMessage = "Por favor, digite o numero de parcelas desejado")]
+        [Display(Name = "Parcelas")]
+        public int LoanInstallments { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Data do inicio do Emprestimo")]
+        public DateTime DateStartLoan { get; set; }
+
+        public enum LoanType 
+        {
+            Pessoal,
+            Automotivo,
+            Consignado,
+            Rotativo,
+            Imobiliario
+        }
+
+        List<Installments> Installments { get; set; }
+
+    }
+}
